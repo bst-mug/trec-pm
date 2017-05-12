3fc99abc5709c4973d32234b40395604ac331687
Pablo López-García <plopezgarcia@gmail.com>
2017-05-12 12:50:29 +0200

    Added bulk indexing tests

--- src/test/java/at/medunigraz/imi/bst/medline/MedlineTests.java
+++ src/test/java/at/medunigraz/imi/bst/medline/MedlineTests.java
@@ -1,6 +1,8 @@
 package at.medunigraz.imi.bst.medline;
 
 import org.apache.commons.lang3.StringEscapeUtils;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
@@ -11,6 +13,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -19,7 +22,8 @@ import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class MedlineTests {
 
-    String DATA_FOLDER = "/Users/plopez/Desktop/trec/";
+    //String DATA_FOLDER = "/Users/plopez/Desktop/trec/";
+    String DATA_FOLDER = "/Volumes/PabloSSD/trec/medline_xml_all/";
 
     String LARGE_XML_GZIPPED = DATA_FOLDER + "medline17n0739.xml.gz";
     String LARGE_XML = DATA_FOLDER + "uncompressed/medline17n0739.xml";
@@ -27,6 +31,7 @@ public class MedlineTests {
 
     List<String> SAMPLE_MULTI_XML = Arrays.asList(
                                                 DATA_FOLDER + "uncompressed/medline17n0050.xml",
+                                                DATA_FOLDER + "uncompressed/medline17n0100.xml",
                                                 DATA_FOLDER + "uncompressed/medline17n0189.xml",
                                                 DATA_FOLDER + "uncompressed/medline17n0201.xml",
                                                 DATA_FOLDER + "uncompressed/medline17n0355.xml",
@@ -134,6 +139,23 @@ public class MedlineTests {
     }
 
     @Test
+    public void index30KdocsPlainBulk() throws Exception {
+
+        long startTime = System.currentTimeMillis();
+
+        List<PubMedArticle> pubMedArticles = XmlPubMedArticleSet.getPubMedArticlesFromXml(LARGE_XML);
+
+        long xmlDuration = (System.currentTimeMillis() - startTime);
+
+        System.out.println("PARSE XML: " + xmlDuration/1000 + " secs - " + pubMedArticles.size() + " articles");
+
+        long indexingDuration = indexArticlesBulk(pubMedArticles);
+
+        System.out.println("TOTAL: " + TimeUnit.MILLISECONDS.toSeconds(xmlDuration + indexingDuration) + " seconds");
+
+    }
+
+    @Test
     public void index300KdocsPlain() throws Exception {
 
         long startTime = System.currentTimeMillis();
@@ -144,6 +166,54 @@ public class MedlineTests {
             System.out.println(pubMedArticles);
             indexArticles(pubMedArticles);
         }
+
+        System.out.println("TOTAL: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime) + " seconds");
+    }
+
+    @Test
+    public void index300KdocsPlainBulk() throws Exception {
+
+        long startTime = System.currentTimeMillis();
+
+        for (String articleXml : SAMPLE_MULTI_XML) {
+            System.out.println(articleXml);
+            List<PubMedArticle> pubMedArticles = XmlPubMedArticleSet.getPubMedArticlesFromXml(articleXml);
+            //System.out.println(pubMedArticles);
+            indexArticlesBulk(pubMedArticles);
+        }
+
+        System.out.println("TOTAL: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime) + " seconds");
+    }
+
+    @Test
+    public void index300KdocsPlainBulkCompressed() throws Exception {
+
+        long startTime = System.currentTimeMillis();
+
+        for (String articleXml : SAMPLE_MULTI_XML) {
+            articleXml = articleXml.replace("uncompressed/", "") + ".gz";
+            System.out.println(articleXml);
+            List<PubMedArticle> pubMedArticles = XmlPubMedArticleSet.getPubMedArticlesFromGzippedXml(articleXml);
+            //System.out.println(pubMedArticles);
+            indexArticlesBulk(pubMedArticles);
+        }
+
+        System.out.println("TOTAL: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime) + " seconds");
+    }
+
+    @Test
+    public void indexAllUncompressed() throws Exception {
+
+        long startTime = System.currentTimeMillis();
+        System.out.println("START: " + startTime);
+
+        for (int fileNumber = 1; fileNumber <= 889; fileNumber++) {
+            String articleXml = String.format(DATA_FOLDER + "uncompressed/medline17n0%03d.xml", fileNumber);
+            System.out.println(articleXml);
+            List<PubMedArticle> pubMedArticles = XmlPubMedArticleSet.getPubMedArticlesFromXml(articleXml);
+            indexArticlesBulk(pubMedArticles);
+        }
+        System.out.println("END: " + System.currentTimeMillis());
 
         System.out.println("TOTAL: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime) + " seconds");
     }
@@ -175,6 +245,47 @@ public class MedlineTests {
         long indexingDuration = (System.currentTimeMillis() - startTime);
 
         System.out.println("INDEXING TIME: " + indexingDuration/1000 + " secs - " + pubMedArticles.size() + " articles");
+
+        return indexingDuration;
+
+    }
+
+    private long indexArticlesBulk(List<PubMedArticle> pubMedArticles) throws Exception{
+
+        long startTime = System.currentTimeMillis();
+
+        TransportAddress address =
+                new InetSocketTransportAddress(
+                        InetAddress.getByName("localhost"), 9300);
+
+        Client client = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(address);
+
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+        for (PubMedArticle article: pubMedArticles) {
+
+            bulkRequest.add(client.prepareIndex("medline", "medline", article.pubMedId)
+                    .setSource(jsonBuilder()
+                            .startObject()
+                            .field("title", StringEscapeUtils.escapeJson(article.docTitle))
+                            .field("abstract", StringEscapeUtils.escapeJson(article.docAbstract))
+                            .field("meshTags", article.meshTags)
+                            .endObject()
+                    )
+            );
+        }
+
+        BulkResponse bulkResponse = bulkRequest.get();
+
+        if (bulkResponse.hasFailures()) {
+            System.out.println("Failures!!!!");
+        }
+
+        client.close();
+
+        long indexingDuration = (System.currentTimeMillis() - startTime);
+
+        System.out.println("INDEXING TIME BULK: " + indexingDuration/1000 + " secs - " + pubMedArticles.size() + " articles");
 
         return indexingDuration;
 
