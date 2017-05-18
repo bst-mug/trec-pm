dd0077eb3bcf483ac2d57dd87d36f5c6ad2b6ede
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-05-18 21:22:27 +0200

    Creates ConnectionUtils
    - Util class that hold static methods that test the connection to the
    Elastic Search.

--- src/test/java/at/medunigraz/imi/bst/trec/search/ElasticClientFactoryTest.java
+++ src/test/java/at/medunigraz/imi/bst/trec/search/ElasticClientFactoryTest.java
@@ -3,22 +3,19 @@ package at.medunigraz.imi.bst.trec.search;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-import java.io.IOException;
-import java.net.Socket;
-
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.junit.Assume;
 import org.junit.Test;
 
-import at.medunigraz.imi.bst.config.TrecConfig;
+import at.medunigraz.imi.bst.trec.utils.ConnectionUtils;
 
 public class ElasticClientFactoryTest {
 
 	public ElasticClientFactoryTest() {
 		// There must be an available server
-		Assume.assumeTrue(checkOpenPort(TrecConfig.ELASTIC_HOSTNAME, TrecConfig.ELASTIC_PORT));
+		Assume.assumeTrue(ConnectionUtils.checkElasticOpenPort());
 	}
 
 	@Test
@@ -38,20 +35,5 @@ public class ElasticClientFactoryTest {
 		ClusterHealthStatus status = health.getStatus();
 		int actual = status.compareTo(ClusterHealthStatus.RED);
 		assertNotEquals(0, actual); // 0 = RED
-	}
-
-	/**
-	 * Checks whether a given port is open on a server
-	 * 
-	 * @param hostname
-	 * @param port
-	 * @return
-	 */
-	private boolean checkOpenPort(String hostname, int port) {
-		try (Socket ignored = new Socket(hostname, port)) {
-			return true;
-		} catch (IOException ignored) {
-			return false;
-		}
 	}
 }
