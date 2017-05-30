35fe0175d0ab0b36d9927e2ff797e045b384593b
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-05-30 19:02:32 +0200

    Introducing TopicSet
    TopicSet encloses a set of topics that can be loaded from a XML file.

---
+++ src/test/java/at/medunigraz/imi/bst/trec/model/TopicSetTest.java
@@ -0,0 +1,29 @@
+package at.medunigraz.imi.bst.trec.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class TopicSetTest {
+
+	@Test
+	public void testFromXML() {
+		File topicsFile = new File(getClass().getResource("/topics/topics2017.xml").getPath());
+		Set<Topic> topics = (new TopicSet(topicsFile)).getTopics();
+		
+		assertEquals(30, topics.size());
+		assertTrue(topics.contains(new Topic().withNumber(30)));
+		
+		Topic firstTopic = topics.iterator().next();
+		assertEquals(1, firstTopic.getNumber());
+		assertEquals("Liposarcoma", firstTopic.getDisease());
+		assertEquals("CDK4 Amplification", firstTopic.getVariant());
+		assertEquals("38-year-old male", firstTopic.getDemographic());
+		assertEquals("GERD", firstTopic.getOther());
+	}
+
+}
