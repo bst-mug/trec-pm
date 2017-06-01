de5d36199d290acab31c10a6c19d19a792cc1d18
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-06-01 17:23:57 +0200

    Output files follows naming conventions

--- src/main/java/at/medunigraz/imi/bst/trec/RunnerDemo.java
+++ src/main/java/at/medunigraz/imi/bst/trec/RunnerDemo.java
@@ -16,12 +16,12 @@ public class RunnerDemo {
 	private static final Logger LOG = LogManager.getLogger();
 	
 	public static void main(String[] args) {
-		final String id = "example-pmid";
+		final String id = "example", suffix = "pmid";
 
-		File example = new File(StatsWriter.class.getResource("/topics/example.xml").getPath());
+		File example = new File(StatsWriter.class.getResource("/topics/" + id + ".xml").getPath());
 		TopicSet topicSet = new TopicSet(example);
 		
-		File output = new File("results/" + id + ".trec_results");
+		File output = new File("results/" + id + "-" + suffix + ".trec_results");
 		TrecWriter tw = new TrecWriter(output);
 
 		ElasticSearch es = new ElasticSearch();
@@ -30,12 +30,12 @@ public class RunnerDemo {
 		tw.write(resultListSet);
 		tw.close();
 
-		File goldStandard = new File(StatsWriter.class.getResource("/gold-standard/" + id + ".qrels").getPath());
+		File goldStandard = new File(StatsWriter.class.getResource("/gold-standard/" + id + "-" + suffix + ".qrels").getPath());
 		TrecEval te = new TrecEval(goldStandard, output);
 		
 		LOG.debug(te.getMetricsAsString());
 
-		StatsWriter sw = new StatsWriter(new File("stats/stats.csv"));
+		StatsWriter sw = new StatsWriter(new File("stats/" + id + "-" + suffix + ".csv"));
 		sw.write(te.getMetrics());
 		sw.close();
 	}
