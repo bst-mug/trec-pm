b236fd019379fb2f9d4bdea9ac12f08ee946ad8e
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-06-01 18:01:57 +0200

    Adds support for running several collections

--- src/main/java/at/medunigraz/imi/bst/trec/RunnerDemo.java
+++ src/main/java/at/medunigraz/imi/bst/trec/RunnerDemo.java
@@ -14,29 +14,34 @@ import at.medunigraz.imi.bst.trec.search.ElasticSearch;
 
 public class RunnerDemo {
 	private static final Logger LOG = LogManager.getLogger();
-	
+
 	public static void main(String[] args) {
-		final String id = "example", suffix = "pmid";
+		final String[] runIds = { "example", "extra" };
+		final String suffix = "pmid";
 
-		File example = new File(StatsWriter.class.getResource("/topics/" + id + ".xml").getPath());
-		TopicSet topicSet = new TopicSet(example);
-		
-		File output = new File("results/" + id + "-" + suffix + ".trec_results");
-		TrecWriter tw = new TrecWriter(output);
+		for (String id : runIds) {
+			LOG.info("Running collection '" + id + "'...");
+			File example = new File(StatsWriter.class.getResource("/topics/" + id + ".xml").getPath());
+			TopicSet topicSet = new TopicSet(example);
 
-		ElasticSearch es = new ElasticSearch();
-		Set<ResultList> resultListSet = es.query(topicSet);
-		
-		tw.write(resultListSet);
-		tw.close();
+			File output = new File("results/" + id + "-" + suffix + ".trec_results");
+			TrecWriter tw = new TrecWriter(output);
 
-		File goldStandard = new File(StatsWriter.class.getResource("/gold-standard/" + id + "-" + suffix + ".qrels").getPath());
-		TrecEval te = new TrecEval(goldStandard, output);
-		
-		LOG.debug(te.getMetricsAsString());
+			ElasticSearch es = new ElasticSearch();
+			Set<ResultList> resultListSet = es.query(topicSet);
 
-		StatsWriter sw = new StatsWriter(new File("stats/" + id + "-" + suffix + ".csv"));
-		sw.write(te.getMetrics());
-		sw.close();
+			tw.write(resultListSet);
+			tw.close();
+
+			File goldStandard = new File(
+					StatsWriter.class.getResource("/gold-standard/" + id + "-" + suffix + ".qrels").getPath());
+			TrecEval te = new TrecEval(goldStandard, output);
+
+			LOG.trace(te.getMetricsAsString());
+
+			StatsWriter sw = new StatsWriter(new File("stats/" + id + "-" + suffix + ".csv"));
+			sw.write(te.getMetrics());
+			sw.close();
+		}
 	}
 }
