10e8d2c9ad1801dc86f62732cfe55101f068001b
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-06-14 18:25:41 +0200

    Makes JSON queries be printed also in debug mode

--- src/main/java/at/medunigraz/imi/bst/trec/search/ElasticSearch.java
+++ src/main/java/at/medunigraz/imi/bst/trec/search/ElasticSearch.java
@@ -50,7 +50,7 @@ public class ElasticSearch implements SearchEngine {
 	
 	private List<Result> query(QueryBuilder qb) {
 		SearchRequestBuilder searchRequestBuilder = client.prepareSearch().setQuery(qb).setSize(1000);
-		LOG.trace(searchRequestBuilder.toString());
+		LOG.debug(searchRequestBuilder.toString());
 		
 		SearchResponse response = searchRequestBuilder.get();
 		LOG.trace(JsonUtils.prettify(response.toString()));
