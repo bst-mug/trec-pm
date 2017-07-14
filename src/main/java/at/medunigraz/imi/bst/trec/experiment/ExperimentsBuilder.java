7a12b43ab408f884a308c7249c130a2b25b7d77c
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-07-14 12:13:37 +0200

    Introduces handy methods to build experiments
    So that decorators don't need to be instantiated manually.

--- src/main/java/at/medunigraz/imi/bst/trec/experiment/ExperimentsBuilder.java
+++ src/main/java/at/medunigraz/imi/bst/trec/experiment/ExperimentsBuilder.java
@@ -1,9 +1,15 @@
 package at.medunigraz.imi.bst.trec.experiment;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
+import at.medunigraz.imi.bst.trec.model.Gene;
+import at.medunigraz.imi.bst.trec.query.ElasticSearchQuery;
+import at.medunigraz.imi.bst.trec.query.GeneExpanderQueryDecorator;
 import at.medunigraz.imi.bst.trec.query.Query;
+import at.medunigraz.imi.bst.trec.query.TemplateQueryDecorator;
+import at.medunigraz.imi.bst.trec.query.WordRemovalQueryDecorator;
 
 public class ExperimentsBuilder {
 
@@ -15,7 +21,7 @@ public class ExperimentsBuilder {
 	}
 
 	public ExperimentsBuilder newExperiment() {
-		addBuilding();
+		validate();
 		buildingExp = new Experiment();
 		return this;
 	}
@@ -27,24 +33,57 @@ public class ExperimentsBuilder {
 		return this;
 	}
 
+	@Deprecated
 	public ExperimentsBuilder withId(String id) {
 		buildingExp.setExperimentId(id);
 		return this;
 	}
 
+	@Deprecated
 	public ExperimentsBuilder withDecorator(Query decorator) {
 		buildingExp.setDecorator(decorator);
 		return this;
 	}
 
+	public ExperimentsBuilder withTemplate(File template) {
+		Query previousDecorator = buildingExp.getDecorator();
+		buildingExp.setDecorator(new TemplateQueryDecorator(template, previousDecorator));
+		return this;
+	}
+
+	public ExperimentsBuilder withWordRemoval() {
+		Query previousDecorator = buildingExp.getDecorator();
+		buildingExp.setDecorator(new WordRemovalQueryDecorator(previousDecorator));
+		return this;
+	}
+
+	public ExperimentsBuilder withGeneExpansion(Gene.Field[] expandTo) {
+		Query previousDecorator = buildingExp.getDecorator();
+		buildingExp.setDecorator(new GeneExpanderQueryDecorator(expandTo, previousDecorator));
+		return this;
+	}
+
+	public ExperimentsBuilder withGoldStandard(Experiment.GoldStandard gold) {
+		buildingExp.setGoldStandard(gold);
+		return this;
+	}
+
+	public ExperimentsBuilder withTarget(Experiment.Task task) {
+		buildingExp.setTask(task);
+		buildingExp.setDecorator(new ElasticSearchQuery(buildingExp.getTaskName()));
+		return this;
+	}
+
 	public Set<Experiment> build() {
-		addBuilding();
+		validate();
 		return experiments;
 	}
 
-	private void addBuilding() {
+	private void validate() {
 		if (buildingExp != null) {
 			this.experiments.add(buildingExp);
+			return;
 		}
+
 	}
 }
