a99567b18a453275fcb2ddef50ce1e814dcc8f5d
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-07-18 12:07:29 +0200

    Improves regex so that it matches trials without exclusion criteria
    This fixes #92.

--- src/test/java/at/medunigraz/imi/bst/clinicaltrial/ClinicalTrialTest.java
+++ src/test/java/at/medunigraz/imi/bst/clinicaltrial/ClinicalTrialTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 
 import java.io.File;
 
@@ -21,7 +22,21 @@ public class ClinicalTrialTest {
         assertThat(trial.minAge, is(18));
         assertThat(trial.maxAge, is(65));
         assertThat(trial.inclusion, containsString("Endstage"));
+        assertThat(trial.inclusion, containsString("survival"));
         assertThat(trial.exclusion, containsString("Multiple"));
+        assertThat(trial.exclusion, containsString("individual"));
+        
+        
+        xmlFile = new File(getClass().getResource("/data/clinicaltrials-samples/NCT02912559.xml").getFile());
+
+         trial = ClinicalTrial.fromXml(xmlFile.getAbsolutePath());
+
+        assertThat(trial.sex, contains("female", "male"));
+        assertThat(trial.minAge, is(18));
+        assertThat(trial.maxAge, is(100));
+        assertThat(trial.inclusion, containsString("Histologically"));
+        assertThat(trial.inclusion, containsString("leucovorin"));
+        assertThat(trial.exclusion, isEmptyOrNullString());
     }
 
 }
