5fa9393780e280a0ad916b9be3d61941482e6341
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-07-18 18:46:28 +0200

    Avoids PATH override

--- scripts/index-clinical-trials.sh
+++ scripts/index-clinical-trials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH=/mnt/trec/data/clinicaltrials/extracted
-#PATH=/home/info/trec/data/clinicaltrials
+DOCS=/mnt/trec/data/clinicaltrials/extracted
+#DOCS=/home/info/trec/data/clinicaltrials
 
-nohup mvn -DskipTests=true install exec:java -Dexec.mainClass="at.medunigraz.imi.bst.clinicaltrial.Indexing" -Dexec.args="$PATH" | tee clinicaltrials.log
+nohup mvn -DskipTests=true install exec:java -Dexec.mainClass="at.medunigraz.imi.bst.clinicaltrial.Indexing" -Dexec.args="$DOCS" | tee clinicaltrials.log
--- scripts/index-medline.sh
+++ scripts/index-medline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PATH=/mnt/trec/data/medline/extracted
-#PATH=/home/info/trec/data/medline/compressed
+DOCS=/mnt/trec/data/medline/extracted
+#DOCS=/home/info/trec/data/medline/compressed
 
 # Fixes Java heap space error
 export MAVEN_OPTS=-Xmx4g
 
-nohup mvn -DskipTests=true install exec:java -Dexec.mainClass="at.medunigraz.imi.bst.medline.Indexing" -Dexec.args="$PATH 1" | tee medline.log
+nohup mvn -DskipTests=true install exec:java -Dexec.mainClass="at.medunigraz.imi.bst.medline.Indexing" -Dexec.args="$DOCS 1" | tee medline.log
