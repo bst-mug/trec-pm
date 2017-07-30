38d2d7f0bfb6e38c067424352a0d9f6ca8fd22b5
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2017-07-30 15:54:19 +0200

    Clinical Trials experiments
    Adds a new ClinicalTrialsExperimenter class so that we can record and
    compare performance of different strategies.
    
    Our first strategy differs from baseline by excluding results with
    comorbidities (from the 'others' field) in the exclusion criteria. This
    fixes #93.

---
+++ src/main/java/at/medunigraz/imi/bst/trec/ClinicalTrialsExperimenter.java
@@ -0,0 +1,40 @@
+package at.medunigraz.imi.bst.trec;
+
+import java.io.File;
+import java.util.Set;
+
+import at.medunigraz.imi.bst.trec.experiment.Experiment;
+import at.medunigraz.imi.bst.trec.experiment.ExperimentsBuilder;
+
+public class ClinicalTrialsExperimenter {
+	public static void main(String[] args) {
+		final File baselineTemplate = new File(
+				ClinicalTrialsExperimenter.class.getResource("/templates/baseline-ct.json").getFile());
+		final File mustNotOtherTemplate = new File(
+				ClinicalTrialsExperimenter.class.getResource("/templates/must-not-other.json").getFile());
+
+		ExperimentsBuilder builder = new ExperimentsBuilder();
+
+		builder.newExperiment().withGoldStandard(Experiment.GoldStandard.EXTRA)
+				.withTarget(Experiment.Task.CLINICAL_TRIALS).withTemplate(baselineTemplate);
+
+		builder.newExperiment().withGoldStandard(Experiment.GoldStandard.EXTRA)
+				.withTarget(Experiment.Task.CLINICAL_TRIALS).withTemplate(mustNotOtherTemplate);
+
+		Set<Experiment> experiments = builder.build();
+
+		for (Experiment exp : experiments) {
+			exp.start();
+			try {
+				exp.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		for (Experiment exp : experiments) {
+
+		}
+	}
+
+}
