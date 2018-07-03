0433af1463ac20fdaa4837253a040676b9e774ef
Michel Oleynik <michelole@gmail.com>
2018-07-03 18:34:35 +0200

    Fix a NullPointerException when running RunnerDemo
    Due to pointing to a wrong directory.

--- src/main/java/at/medunigraz/imi/bst/trec/RunnerDemo.java
+++ src/main/java/at/medunigraz/imi/bst/trec/RunnerDemo.java
@@ -10,7 +10,7 @@ public class RunnerDemo {
 	public static void main(String[] args) {
 		final File pmTemplate = new File(RunnerDemo.class.getResource("/templates/biomedical_articles/negative-boost-keywords.json").getFile());
 
-		final File ctTemplate = new File(RunnerDemo.class.getResource("/templates/biomedical_articles/improved-ct.json").getFile());
+		final File ctTemplate = new File(RunnerDemo.class.getResource("/templates/clinical_trials/improved-ct.json").getFile());
 
 		ExperimentsBuilder builder = new ExperimentsBuilder();
 
