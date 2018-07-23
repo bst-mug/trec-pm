8411ed8e50309c0d9c90010d5a666d99b05601d1
Michel Oleynik <michelole@gmail.com>
2018-07-23 15:56:42 +0200

    Add support for recursive subtemplates (refs #16)

--- src/main/java/at/medunigraz/imi/bst/trec/query/SubTemplateQueryDecorator.java
+++ src/main/java/at/medunigraz/imi/bst/trec/query/SubTemplateQueryDecorator.java
@@ -6,30 +6,34 @@ import java.util.regex.Pattern;
 
 public class SubTemplateQueryDecorator extends TemplateQueryDecorator {
 
+    /**
+     * Matches and captures regular filenames enclosed in double curly braces, e.g. "{{positive_boosters.json}}"
+     */
+    private static final Pattern TEMPLATE_PATTERN = Pattern.compile("\\{\\{(\\w+\\.json)\\}\\}");
+
+    private static final File SUBTEMPLATES_FOLDER = new File(SubTemplateQueryDecorator.class.getResource("/templates/subtemplates/").getFile());
+
     public SubTemplateQueryDecorator(File template, Query decoratedQuery) {
         super(template, decoratedQuery);
     }
 
     @Override
     protected void loadTemplate() {
-        String templateString = readTemplate(template);
+        setJSONQuery(recursiveLoadTemplate(template));
+    }
 
-        // Matches regular filenames enclosed in double curly braces, e.g. "{{positive_boosters.json}}"
-        final Pattern subtemplates = Pattern.compile("\\{\\{(\\w+\\.json)\\}\\}");
+    private String recursiveLoadTemplate(File file) {
+        String templateString = readTemplate(file);
 
         StringBuffer sb = new StringBuffer();
 
-        Matcher matcher = subtemplates.matcher(templateString);
+        Matcher matcher = TEMPLATE_PATTERN.matcher(templateString);
         while (matcher.find()) {
             String filename = matcher.group(1);
-
-            String url = this.getClass().getResource(String.format("/templates/subtemplates/%s", filename)).getFile();
-            String subtemplate = readTemplate(new File(url));
-
-            matcher.appendReplacement(sb, subtemplate);
+            matcher.appendReplacement(sb, recursiveLoadTemplate(new File(SUBTEMPLATES_FOLDER, filename)));
         }
         matcher.appendTail(sb);
 
-        setJSONQuery(sb.toString());
+        return sb.toString();
     }
 }
