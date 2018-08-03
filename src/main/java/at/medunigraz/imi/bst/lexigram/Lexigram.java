ccfba846fa491dff3c522b889e0a8be259c2d454
Michel Oleynik <michelole@gmail.com>
2018-08-03 12:08:15 +0200

    Add an auxiliary main method to Lexigram API to debug results

--- src/main/java/at/medunigraz/imi/bst/lexigram/Lexigram.java
+++ src/main/java/at/medunigraz/imi/bst/lexigram/Lexigram.java
@@ -1,6 +1,10 @@
 package at.medunigraz.imi.bst.lexigram;
 
 import at.medunigraz.imi.bst.config.TrecConfig;
+import at.medunigraz.imi.bst.trec.model.Topic;
+import at.medunigraz.imi.bst.trec.model.TopicSet;
+import at.medunigraz.imi.bst.trec.stats.CSVStatsWriter;
+import at.medunigraz.imi.bst.trec.utils.JsonUtils;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -256,5 +260,48 @@ public class Lexigram {
         }
 
         return new JSONObject(Cache.CALLS.get(url));
+    }
+
+    /**
+     * Auxiliary main method to dump all expansions into a file for easier debug.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        final File topicsFile = new File(CSVStatsWriter.class.getResource("/topics/topics2018.xml").getPath());
+
+        TopicSet topicSet = new TopicSet(topicsFile);
+        JSONObject output = createDump(topicSet);
+
+        System.out.println(JsonUtils.prettify(output));
+    }
+
+    private static JSONObject createDump(TopicSet topicSet) {
+        JSONObject output = new JSONObject();
+
+        Set<String> diseaseSet = new HashSet<>();
+
+        for (Topic topic : topicSet.getTopics()) {
+            String disease = topic.getDisease();
+
+            // Do not repeat diseases
+            if (diseaseSet.contains(disease)) {
+                continue;
+            }
+            diseaseSet.add(disease);
+
+            String preferredTerm = getPreferredTerm(disease);
+            List<String> synonyms = getSynonymsFromBestConceptMatch(disease);
+            List<String> ancestors = getAncestorsFromBestConceptMatch(disease);
+
+            JSONObject diseaseJson = new JSONObject()
+                    .put("preferredTerm", preferredTerm)
+                    .put("synonyms", synonyms)
+                    .put("ancestors", ancestors);
+
+            output.put(disease, diseaseJson);
+        }
+
+        return output;
     }
 }
